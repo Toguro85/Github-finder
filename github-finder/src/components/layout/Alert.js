7977f26a4d35eeff6508da545f17cf8f5624e852
Abhinav <abhinavshroff98@gmail.com>
2020-01-03 21:41:58 +0530

    Add search and clear

---
+++ github-finder/src/components/layout/Alert.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const Alert = ({ alert }) => {
+  return (
+    <Fragment>
+      {alert !== null && (
+        <div className={`alert alert-${alert.type}`}>
+          <i className="fas fa-info-circle" />
+          {alert.msg}
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+export default Alert;
